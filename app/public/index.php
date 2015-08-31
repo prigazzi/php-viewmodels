154ef23a7718e88ced53e5c5580bfce87d27bbb2
Pablo Rigazzi <p.rigazzi@coolblue.nl>
2015-08-31 16:27:24 +0200

    Moved everything to vendor directory

--- app/public/index.php
+++ app/public/index.php
@@ -2,27 +2,27 @@
 use Solution\View\Factory;
 
 include __DIR__ . '/../../vendor/autoload.php';
-
+    
 $factory = new Factory();
 $factory->param('App.View.SearchPage.template', __DIR__.'/../templates/searchpage.tpl');
 $factory->param('App.View.Product.template', __DIR__.'/../templates/product.tpl');
 
-$factory->singleton("App.View.SearchPage", function() {
-	return new \Solution\View\View($this->param('App.View.SearchPage.template'));
+$factory->singleton("App.View.SearchPage", function () {
+    return new \Solution\View\View($this->param('App.View.SearchPage.template'));
 });
-$factory->singleton('App.View.Pagination', function() {
-	return new \App\View\Pagination();
+$factory->singleton('App.View.Pagination', function () {
+    return new \App\View\Pagination();
 });
-$factory->factory('App.View.Product', function($productModel) {
-	return new \App\View\Product($this->param('App.View.Product.template'), $productModel);
+$factory->factory('App.View.Product', function ($productModel) {
+    return new \App\View\Product($this->param('App.View.Product.template'), $productModel);
 });
-$factory->factory("App.View.SearchResults", function(\Traversable $resourceCollection) {
-	foreach ($resourceCollection as $productResource) {
-		$productModel = new \App\Model\Product($productResource);
-		$view = $this->get('App.View.Product', $productModel);
+$factory->factory("App.View.SearchResults", function (\Traversable $resourceCollection) {
+    foreach ($resourceCollection as $productResource) {
+        $productModel = new \App\Model\Product($productResource);
+        $view = $this->get('App.View.Product', $productModel);
 
-		yield $view;
-	}
+        yield $view;
+    }
 });
 
 // Example on how to instantiate new views
